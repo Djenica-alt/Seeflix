9392a059640047c8ada1be7885f15992d4fe1846
Djenica <djenicaexilus@gmail.com>
2026-03-24 15:35:58 -0400

    Update show_covers, mediaList and top_shows

--- seeflix/src/data/top_shows.js
+++ seeflix/src/data/top_shows.js
@@ -21,4 +21,95 @@ export const top_shows = [
     rating: 9.2,
     type: "tvshow"
   },
+  {
+    id: 4,
+    title: "Bumblebee",
+    cover: "src\\assets\\show_covers\\Bumblebee.jpg",
+    rating: 9.5,
+    type: "tvshow"
+  },
+  {
+    id: 5,
+    title: "House of the Dragon",
+    cover: "src\\assets\\show_covers\\House of the Dragon.avif",
+    rating: 6.2,
+    type: "tvshow"
+  },
+  {
+    id: 6,
+    title: "K-pop Demon Hunters",
+    cover: "src\\assets\\show_covers\\K-pop Demon Hunters.avif",
+    rating: 9.2,
+    type: "tvshow"
+  },
+  {
+    id: 7,
+    title: "Avengers",
+    cover: "src\\assets\\show_covers\\avengers.avif",
+    rating: 9.4,
+    type: "tvshow"
+  },
+  {
+    id: 8,
+    title: "The 100",
+    cover: "src\\assets\\show_covers\\The 100.avif",
+    rating: 8.2,
+    type: "tvshow"
+  },
+  {
+    id: 9,
+    title: "The Vampire Diaries",
+    cover: "src\\assets\\show_covers\\the Vampire Diaries.avif",
+    rating: 8.1,
+    type: "tvshow"
+  },
+  {
+    id: 10,
+    title: "La casa de papel",
+    cover: "src\\assets\\show_covers\\la casa de papel.avif",
+    rating: 10,
+    type: "tvshow"
+  },
+   {
+    id: 11,
+    title: "Wendnesday",
+    cover: "src\\assets\\show_covers\\wendnesday.avif",
+    rating: 9.2,
+    type: "tvshow"
+  },
+   {
+    id: 12,
+    title: "Vis A Vis",
+    cover: "src\\assets\\show_covers\\visavis.jpg",
+    rating: 7.2,
+    type: "tvshow"
+  },
+   {
+    id: 13,
+    title: "One Piece",
+    cover: "src\\assets\\show_covers\\Netflix One Piece.jpg",
+    rating: 9.2,
+    type: "tvshow"
+  },
+   {
+    id: 14,
+    title: "Damsel",
+    cover: "src\\assets\\show_covers\\Damsel Poster.avif",
+    rating: 8.2,
+    type: "tvshow"
+  },
+   {
+    id: 15,
+    title: "Teen Wolf",
+    cover: "src\\assets\\show_covers\\Teen Wolf.avif",
+    rating: 9.2,
+    type: "tvshow"
+  },
+   {
+    id: 10,
+    title: "Lucifer",
+    cover: "src\\assets\\show_covers\\LLucifer.jpg",
+    rating: 9.4,
+    type: "tvshow"
+  }
 ];
--- seeflix/src/data/trending_movies.js
+++ seeflix/src/data/trending_movies.js
@@ -35,4 +35,67 @@ export const trending_movies = [
     rating: 8.4,
     type: "movie"
   },
+  {
+    id: 6,
+    title: "Blue Beetle-2023",
+    cover: "src\\assets\\movie_covers\\Blue Beetle - 2023.jpg",
+    rating: 8.7,
+    type: "movie"
+  },
+  {
+    id: 7,
+    title: "Encanto",
+    cover: "src\\assets\\movie_covers\\Encanto.avif",
+    rating: 8.5,
+    type: "movie"
+  },
+  {
+    id: 8,
+    title: "Jumanji",
+    cover: "src\\assets\\movie_covers\\Jumanji Next Level.avif",
+    rating: 8.3,
+    type: "movie"
+  },
+  {
+    id:9,
+    title:"Maleficent",
+    cover: "src\\assets\\movie_covers\\Maleficent.avif",
+    rating: 7.5,
+    type: "movie"
+  },
+  {
+    id:9,
+    title:"Pirates des Caraibes",
+    cover: "src\\assets\\movie_covers\\Pirates.jpg",
+    rating: 7.5,
+    type: "movie"
+  },
+   {
+    id:9,
+    title:"Titanic",
+    cover: "src\\assets\\movie_covers\\Titanic.avif",
+    rating: 7.5,
+    type: "movie"
+  },
+   {
+    id:9,
+    title:"Zootopia",
+    cover: "src\\assets\\movie_covers\\Zootopia.jpg",
+    rating: 7.5,
+    type: "movie"
+  },
+   {
+    id:9,
+    title:"Venom",
+    cover: "src\\assets\\movie_covers\\Venom.jpg",
+    rating: 8.5,
+    type: "movie"
+  },
+   {
+    id:9,
+    title:"Mulan March 2020",
+    cover: "src\\assets\\movie_covers\\Mulan March 2020.jpg",
+    rating: 7.5,
+    type: "movie"
+  }
 ];
